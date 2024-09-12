9405431efac5db89676bdb0f4548966ce5044253
Leo <tou522@yahoo.com.tw>
2024-09-12 11:00:24 +0800

    新增訓練紀錄頁面及調整查看訓練紀錄按鈕顯示問題

---
+++ PhysicalFit/Controllers/RecordController.cs
@@ -0,0 +1,19 @@
+﻿using PhysicalFit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhysicalFit.Controllers
+{
+    public class RecordController : Controller
+    {
+        private PhFitnessEntities _db = new PhFitnessEntities(); //資料庫
+
+        public ActionResult SessionRecord()
+        {
+            return View();
+        }
+    }
+}
