e55bd23d8ef5841842d93e5585b4b1535d7d2308
Leo <tou522@yahoo.com.tw>
2024-09-13 17:10:53 +0800

    調整訓練檢視紀錄

--- PhysicalFit/Controllers/RecordController.cs
+++ PhysicalFit/Controllers/RecordController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace PhysicalFit.Controllers
 {
@@ -13,8 +14,35 @@ namespace PhysicalFit.Controllers
 
         public ActionResult SessionRecord()
         {
-            return View();
-        }
+            //判斷是否是教練
+            bool isCoach = User.IsInRole("Coach");
 
+            var userId = User.Identity.GetUserId();
+
+            // 查詢運動員的訓練紀錄，假設 SessionRecords 是紀錄表
+            var query = _db.DetectionTrainingRecord.AsQueryable();
+
+            if (isCoach)
+            {
+                // 假設教練可以選擇運動員，並從 ViewBag 選擇的運動員過濾紀錄
+                var selectedAthleteId = ViewBag.SelectedAthleteId as string;
+                if (int.TryParse(selectedAthleteId, out int athleteId)) // 確保選擇的運動員 ID 可以轉換為 int
+                {
+                    query = query.Where(x => x.AthleteID == athleteId);
+                }
+            }
+            else
+            {
+                if (int.TryParse(userId, out int athleteId)) // 確保 userId 可以轉換為 int
+                {
+                    query = query.Where(x => x.AthleteID == athleteId);
+                }
+            }
+
+            // 根據訓練日期排序
+            var records = query.OrderBy(x => x.TrainingDate).ToList();
+
+            return View(records);
+        }
     }
 }
